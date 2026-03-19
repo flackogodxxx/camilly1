bca2623c263c04959766e323e7154ae38a2b66fc
flackogodxxx <anafelipedukico@gmail.com>
2026-03-18 22:16:29 -0300

    Fix base path to relative for local viewing

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/camilly1/',
+  base: './',
   plugins: [react(), tailwindcss()],
 })
